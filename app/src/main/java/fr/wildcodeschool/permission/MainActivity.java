07d56b6b44a1cfb9c887317834893b8d01b985ec
Sébastien <snx.dubois@gmail.com>
2019-04-04 11:45:45 +0200

    permission granted!

--- app/src/main/java/fr/wildcodeschool/permission/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/permission/MainActivity.java
@@ -9,59 +9,30 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1230;
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1230;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        checkContactPermission();
+        checkLocationPermission();
     }
 
-    private void checkContactPermission() {
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS)
+    private void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
-            // Permission not granted : ask for it
+            //Permission not granted : ask for it
             ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.READ_CONTACTS},
-                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
         } else {
-            // Permission granted
-            showContacts();
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // Permission granted
-                    showContacts();
-                } else {
-                    // Permission denied
-                }
-                break;
-            }
-        }
-    }
-
-    private void showContacts() {
-
-        TextView tvContacts = findViewById(R.id.tvContacts);
-        Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-
-        if (cursor.getCount() > 0) {
-            while (cursor.moveToNext()) {
-                String displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-                tvContacts.append(String.format("%s\n", displayName));
-            }
+            //Permission granted
+            Toast.makeText(MainActivity.this, "Access Granted!", Toast.LENGTH_LONG).show();
         }
     }
 }
